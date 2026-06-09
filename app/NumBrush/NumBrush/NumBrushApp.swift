20ac4167bf2cc6a4c8f86e27ce2bd2379c3d9a0c
josigar123 <joseph.gargari@gmail.com>
2026-06-09 12:04:51 +0200

    add(app): ass switf app

---
+++ app/NumBrush/NumBrush/NumBrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumBrushApp.swift
+//  NumBrush
+//
+//  Created by Joseph Gargari on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct NumBrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
